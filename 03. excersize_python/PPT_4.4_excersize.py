38b997726bcaa2d0f24a7a3572d6a6759558fd39
fern <49337525+haidanliu19@users.noreply.github.com>
2020-01-14 09:26:56 +0900

    Create PPT_4.4_excersize.py

---
+++ 03. excersize_python/PPT_4.4_excersize.py
@@ -0,0 +1,68 @@
+#179
+#1.
+def is_odd(number):
+    if number % 2 == 1:
+        return True
+    else:
+        return False
+    
+#2.
+def avg_numbers(*args):
+    result = 0
+    for i in args:
+        result += i
+    return result/len(args)
+
+print(avg_numbers(1,2))       #1.5
+print(avg_numbers(1,2,3,4,5)) #3.0
+    
+#3.
+input1 = input("첫번째 숫자를 입력하세요:")#3
+input2 = input("첫번째 숫자를 입력하세요:")#6
+
+total = input1 + input2
+print("두 수의 합은 %s입니다." % total)#36
+
+####수정후
+input1 = int(input("첫번째 숫자를 입력하세요:"))#3
+input2 = int(input("첫번째 숫자를 입력하세요:"))#6
+
+total = input1 + input2
+print("두 수의 합은 %s입니다." % total)#9
+
+
+#4.
+print("you" "need" "python")
+print("you"+"need"+"python")
+print("you","need","python")
+print("".join(["you","need","python"]))
+    
+#5.
+f1 = open("test.txt","w")
+f1.write("Life is too short")
+
+f1.close()
+
+f2 = open("test.txt",'r')
+print(f2.read())
+
+f2.close()
+
+    
+#6.
+user_input = input("저장할 내용을 입력하세요.")
+f = open('test.txt','a')
+f.write(user_input)
+f.write("\n")
+f.close()
+    
+#7.
+f = open('test.txt','r')
+body = f.read()
+f.close()
+
+body = body.replace("java","python")
+
+f= open('test.txt','w')
+f.write(body)
+f.close()
