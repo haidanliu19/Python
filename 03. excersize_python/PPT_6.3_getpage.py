b6e5ad47557991bab1c2418f355727b3be613c0f
fern <49337525+haidanliu19@users.noreply.github.com>
2020-01-14 09:27:58 +0900

    Create PPT_6.3_getpage.py

---
+++ 03. excersize_python/PPT_6.3_getpage.py
@@ -0,0 +1,19 @@
+def getTotalPage(m,n):
+    return m //n +1
+
+print(getTotalPage(5,10))
+print(getTotalPage(15,10))
+print(getTotalPage(25,10))
+print(getTotalPage(30,10))
+
+#실패 케이스 해결하기
+def getTotalPage(m,n):
+    if m % n ==0:
+        return m // n
+    else:
+        return m // n +1
+    
+print(getTotalPage(5,10))
+print(getTotalPage(15,10))
+print(getTotalPage(25,10))
+print(getTotalPage(30,10))
