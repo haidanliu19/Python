a28adc7ec964ffe04b548532dedf554fb0758276
fern <49337525+haidanliu19@users.noreply.github.com>
2020-01-14 09:27:52 +0900

    Create PPT_6.1_program.py

---
+++ 03. excersize_python/PPT_6.1_program.py
@@ -0,0 +1,21 @@
+#ggd01.py
+def GuGu(n):
+    result = []#결과값을 저장할 리스트인 result
+    i = 1
+    while i < 10:
+        result.append(n*i)
+        i = i+1
+
+    return result
+
+#result = GuGu(2)
+print(GuGu(2))
+
+i = 1
+while i < 10:
+    print(i)
+    i = i+1
+
+
+
+
