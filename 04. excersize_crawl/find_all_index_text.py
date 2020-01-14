4f3cdd618efb19de67bfcd7c3ec3617165595d4e
fern <49337525+haidanliu19@users.noreply.github.com>
2020-01-14 09:33:50 +0900

    Create find_all_index_text.py

---
+++ 04. excersize_crawl/find_all_index_text.py
@@ -0,0 +1,22 @@
+import bs4
+html_str = """
+<html>
+    <body>
+        <ul>
+            <li>hello</li>
+            <li>bye</li>
+            <li>welcome</li>
+        </ul>
+    </body>
+</html>
+"""
+bs_obj = bs4.BeautifulSoup(html_str,"html.parser")
+ul = bs_obj.find("ul")
+li = ul.findAll("li")
+print(li[1].text)
+
+for i in li:
+    print(i.text)
+
+for i in range(len(li)):
+    print(li[i].text)
